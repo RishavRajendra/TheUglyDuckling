813d09ede1f3178365a76dda68e540a401572eea
Rishav Rajendra <rishavrajendra@gmail.com>
2019-02-26 01:07:15 -0600

    Angle detection support

--- run_ssd_demo.py
+++ run_ssd_demo.py
@@ -1,10 +1,35 @@
 from vision.mobilenetv2_ssd_lite import create_mobilenetv2_ssd_lite, create_mobilenetv2_ssd_lite_predictor
 from vision.utils.misc import Timer
+from constants import CENTER_LINEx1y1, CENTER_LINEx2y2
+import math
 import cv2
 import sys
 
+import warnings
+warnings.filterwarnings('ignore')
+    
+"""
+If an obstacle is detected, draw a line from CENTER_LINEx2y2
+to the center of the detected object
+Calculate angle from the center line to align the robot with the obstacle
+angle = arctan(m2-m1/(1+m1m2))
+:param image: image to draw lines on
+:param x1: Object x1 coordinate
+:param y1: Object y1 coordinate
+:param x2: Object x2 coordinate
+:param y2: Object y2 coordinate
+:return angle: (int)
+"""
+def detectObjects(image, x1, y1, x2, y2):
+    center = ((x1 + x2)/2, (y1 + y2)/2)
+    cv2.line(image, center, CENTER_LINEx2y2, (0, 0, 0))
+    m1 = (CENTER_LINEx1y1[1] - CENTER_LINEx2y2[1])/((CENTER_LINEx1y1[0] - CENTER_LINEx2y2[0])+0.001)
+    m2 = (center[1] - CENTER_LINEx2y2[1])/(center[0] - CENTER_LINEx2y2[0])
+    div = (m2 - m1) / (1 + (m1 * m2))
+    return int(math.degrees(math.atan(div)))
+
 if len(sys.argv) < 5:
-    print('Usage: python run_ssd_example.py <net type>  <model path> <image path>')
+    print('Usage: python run_ssd_example.py <net type> <model path> <label path> <image path>')
     sys.exit(0)
 net_type = sys.argv[1]
 model_path = sys.argv[2]
@@ -27,20 +52,33 @@ else:
     sys.exit(1)
 
 orig_image = cv2.imread(image_path)
+
 image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB)
 boxes, labels, probs = predictor.predict(image, 10, 0.4)
 
+
+cv2.line(orig_image, CENTER_LINEx1y1, CENTER_LINEx2y2, (0, 0, 0))
+
 for i in range(boxes.size(0)):
+    interiorAngle = 0
     box = boxes[i, :]
     cv2.rectangle(orig_image, (box[0], box[1]), (box[2], box[3]), (255, 255, 0), 4)
-    #label = f"""{voc_dataset.class_names[labels[i]]}: {probs[i]:.2f}"""
-    label = f"{class_names[labels[i]]}: {probs[i]:.2f}"
+
+    #If an obstacle is detected, find the angle from the center
+    # if class_names[labels[i]] == 'obstacle':
+        # interiorAngle = int(obstacleDetected(orig_image, box[0], box[1], box[2], box[3]))
+    angle = detectObjects(orig_image, box[0], box[1], box[2], box[3])
+    
+    # label = f"{class_names[labels[i]]}: {probs[i]:.2f}, {interiorAngle}"
+    label = f"{angle}"
+
     cv2.putText(orig_image, label,
                 (box[0] + 20, box[1] + 40),
                 cv2.FONT_HERSHEY_SIMPLEX,
                 0.5,  # font scale
                 (255, 0, 255),
                 1)  # line type
+
 path = "run_ssd_example_output.jpg"
 cv2.imwrite(path, orig_image)
 print(f"Found {len(probs)} objects. The output image is {path}")
