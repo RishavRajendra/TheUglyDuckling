8f972767cd1fb53a9af595c65b2dd736cf18bcc9
Rishav Rajendra <rishavrajendra@gmail.com>
2019-04-01 18:37:03 -0500

    Pick up beta

--- get_stats_from_image.py
+++ get_stats_from_image.py
@@ -6,7 +6,10 @@ __status__ = "Development"
 
 import cv2
 import math, time
-from constants import CENTER_LINEx1y1, CENTER_LINEx2y2, PIXEL_PER_MM, ERROR_VAL, FOCAL_LENGTH, OBSTACLE_HEIGHT, TARGET_HEIGHT, MOTHERSHIP_SIDE_HEIGHT, MOTHERSHIP_SLOPE_HEIGHT
+from constants import CENTER_LINEx1y1, CENTER_LINEx2y2, PIXEL_PER_MM, \
+ERROR_VAL, FOCAL_LENGTH, OBSTACLE_HEIGHT, TARGET_HEIGHT, MOTHERSHIP_SIDE_HEIGHT, \
+MOTHERSHIP_SLOPE_HEIGHT, CENTER_DISTANCE_UP, CENTER_DISTANCE_DOWN
+from operator import itemgetter
 
 # angle = arctan((m2-m1)/(1+(m1*m2)))
 def get_angle(image, xmin, ymin, xmax, ymax):
@@ -74,8 +77,53 @@ def get_sensor_data(serial):
         return 0
 
 # Returns [object_name, angle, inches]
-def get_data(processed_frame, classes, boxes, scores):
+def get_data(pic_q):
+    processed_frame, classes, boxes, scores = pic_q.get()
     result = []
+    for i, b in enumerate(boxes[0]):
+        if scores[0][i] > 0.5:
+            inches = 0
+            #extract pixel coordinates of detected objects
+            ymin = boxes[0][i][0]*300
+            xmin = boxes[0][i][1]*300
+            ymax = boxes[0][i][2]*300
+            xmax = boxes[0][i][3]*300
+
+            # Calculate mid_pount of the detected object
+            mid_x = (xmax + xmin) / 2
+            mid_y = (ymax + ymin) / 2
+            
+            # Calculate height of the object located for distance measurements
+            height_of_object_pixels = ymax - ymin
+            
+            """
+            class 7 is the obstacle with the ping-pong ball on top
+            class 8 is the side of the mothership
+            class 9 is the slope of the mothership
+            """
+            if classes[0][i] == 7:
+                inches = get_distance(0, height_of_object_pixels)
+            elif classes[0][i] == 8:
+                inches = get_distance(2, height_of_object_pixels)
+            elif classes[0][i] == 9:
+                inches = get_distance(3, height_of_object_pixels)
+            angle = get_angle(processed_frame, xmin, ymin, xmax, ymax)
+            print('{} detected at {}{} {} inches away. Midpoint:({},{}). Height: {}'.format(classes[0][i],angle,chr(176),inches, int(mid_x), int(mid_y), int(height_of_object_pixels)))
+            result.append([int(classes[0][i]), angle, inches])
+    return result
+
+# This function returns the closest target to the robot
+# Used in approach and pickUp
+def get_closest_target(pic_q, mid_point=False):
+    processed_frame, classes, boxes, scores = pic_q.get()
+    result = []
+    
+    # return [0, 0, 100] if nothing is found
+    if mid_point is True:
+        result.append([0, 0, 100, (100, 100)])
+    else:
+        result.append([0, 0, 100])
+    
     for i, b in enumerate(boxes[0]):
         if scores[0][i] > 0.3:
             inches = 0
@@ -94,22 +142,31 @@ def get_data(processed_frame, classes, boxes, scores):
             
             """
             classes 1 to 6 are the six target blocks
-            class 7 is the obstacle with the ping-pong ball on top
-            class 8 is the side of the mothership
-            class 9 is the slope of the mothership
             """
             if classes[0][i] > 0 and classes[0][i] < 7:
                 inches = get_distance(1, height_of_object_pixels)
-            elif classes[0][i] == 7:
-                inches = get_distance(0, height_of_object_pixels)
-            elif classes[0][i] == 8:
-                inches = get_distance(2, height_of_object_pixels)
-            elif classes[0][i] == 9:
-                inches = get_distance(3, height_of_object_pixels)
-            angle = get_angle(processed_frame, xmin, ymin, xmax, ymax)
-            print('{} detected at {}{} {} inches away. Midpoint:({},{}). Height: {}'.format(classes[0][i],angle,chr(176),inches, int(mid_x), int(mid_y), int(height_of_object_pixels)))
-            result.append([int(classes[0][i]), angle, inches])
-    return result
+                angle = get_angle(processed_frame, xmin, ymin, xmax, ymax)
+                
+                if classes[0][i] == 1:
+                    print('blockA detected at {}{} {} inches away. Midpoint:({},{}). Height: {}'.format(angle,chr(176),inches, int(mid_x), int(mid_y), int(height_of_object_pixels)))
+                elif classes[0][i] == 2:
+                    print('blockB detected at {}{} {} inches away. Midpoint:({},{}). Height: {}'.format(angle,chr(176),inches, int(mid_x), int(mid_y), int(height_of_object_pixels)))
+                elif classes[0][i] == 3:
+                    print('blockC detected at {}{} {} inches away. Midpoint:({},{}). Height: {}'.format(angle,chr(176),inches, int(mid_x), int(mid_y), int(height_of_object_pixels)))
+                elif classes[0][i] == 4:
+                    print('blockD detected at {}{} {} inches away. Midpoint:({},{}). Height: {}'.format(angle,chr(176),inches, int(mid_x), int(mid_y), int(height_of_object_pixels)))
+                elif classes[0][i] == 5:
+                    print('blockE detected at {}{} {} inches away. Midpoint:({},{}). Height: {}'.format(angle,chr(176),inches, int(mid_x), int(mid_y), int(height_of_object_pixels)))
+                elif classes[0][i] == 6:
+                    print('blockF detected at {}{} {} inches away. Midpoint:({},{}). Height: {}'.format(angle,chr(176),inches, int(mid_x), int(mid_y), int(height_of_object_pixels)))
+                
+                if mid_point is True:
+                    result.append([int(classes[0][i]), angle, inches, (int(mid_x),int(mid_y))])
+                else:
+                    result.append([int(classes[0][i]), angle, inches])
+    result = sorted(result, key=itemgetter(2))
+    return result[0]
+        
     
 def get_midpoint(processed_frame, classes, boxes, scores):
     result = []
@@ -131,3 +188,15 @@ def get_midpoint(processed_frame, classes, boxes, scores):
             
             result.append([int(classes[0][i]), (int(mid_x), int(mid_y))])
     return result
+
+def corrected_angle(angle, dist, cam_up=True):
+    cd = CENTER_DISTANCE_UP if cam_up else CENTER_DISTANCE_DOWN
+    sign = -1 if angle < 0 else 1
+    angle = 180 - abs(angle)
+    a = math.sqrt(math.pow(dist,2) + math.pow(cd, 2) - 2*dist*cd*math.cos(math.radians(angle)))
+    angle_c = math.degrees(math.asin(math.sin(math.radians(angle))*dist/a))
+    angle_b  = 180 -angle - angle_c
+    if angle_c < angle_b:
+        return math.floor(angle_c) * sign
+    
+    return math.floor(angle_b) * sign
