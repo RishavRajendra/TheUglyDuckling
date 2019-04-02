c33e61f8b82a6642d39b8f0a415151270fa2eeb1
Rishav Rajendra <rishavrajendra@gmail.com>
2019-04-01 23:50:18 -0500

    Pickup done

--- pickUp.py
+++ pickUp.py
@@ -33,8 +33,16 @@ def wait_for_button(buttonPin, ledPin):
     print('Executing') 
 
 # TODO: Check if the block is actually picked up
-def check_pick_up():
-    pass
+def check_pick_up(movement, pic_q):
+    success = False
+    movement.cam_down()
+    time.sleep(3)
+    target_id, angle, inches, midpoint = get_closest_target(pic_q, True)
+    if target_id is not 0 and inches < 5:
+        print("Pick up successful")
+        success = True
+    movement.cam_up()
+    return success
 
 def move_to_target(movement, angle, distance):
     movement.turn(angle)
@@ -43,18 +51,20 @@ def move_to_target(movement, angle, distance):
 def move_back_from_target(movement, angle, distance):
     movement.move(rev, distance)
     movement.turn(angle)
-    
-# TODO: Refine pick_up. Take care of edge cases.
+
+def corner_align(movement, pic_q):
+    pass
 
 """
+TODO: Refine pick_up. Take care of edge cases.
 Change log
     -[0.0.1] Benji
         --- Changed sleep from 2 to 1.5; lowest fps is .75 so sleeping
         --- for 1.5 seconds is the minimum delay that guarantees fresh video data
-    -[0.0.1] Risahv
+    -[0.0.2] Rishav
         --- Remove recursion. Not getting satisfying results.
 """
-def pick_up(movement, pic_q, serial):
+def pick_up(movement, pic_q):
     movement.cam_down()
     # Let the imaging catch_up
     time.sleep(3)
@@ -71,19 +81,37 @@ def pick_up(movement, pic_q, serial):
 
             # Get another picture as distance can get wacky at an angle
             target_id, angle, inches, midpoint = get_closest_target(pic_q, True)
-            move_to_target(movement,-1*corrected_angle(angle, inches, False), math.ceil(inches*0.7))
+            # Calculate adjusted angle from the front of the robot instead of the middle of the robot
+            adjustedDistance = math.ceil(inches*0.75)
+            adjustedAngle = corrected_angle(angle, adjustedDistance, False)
+            
+            # Angles reversed because East is 0 and we increase as we go around
+            move_to_target(movement,-1*adjustedAngle, adjustedDistance)
             movement.pickup()
-            move_back_from_target(movement,corrected_angle(angle, inches), math.ceil(inches*0.7))
+            move_back_from_target(movement,adjustedAngle, adjustedDistance)
             # Reverse movements
             movement.turn(correctedAngle)
 
     movement.cam_up()
 
-# TODO: Refine approach. Take care of edge cases.
+# Moves the robot close to the target
+def approach_helper(angle, distance, pic_q, movement):
+    adjustedDistance = math.ceil(distance*0.9)
+    adjustedAngle = corrected_angle(angle, adjustedDistance)
+    # move towards the target
+    # Angles reversed because East is 0 and we increase as we go around
+    move_to_target(movement,-1*adjustedAngle, adjustedDistance)
+    #Realign with block
+    pick_up(movement, pic_q)
+    # Reverse movement from the target
+    move_back_from_target(movement, adjustedAngle, adjustedDistance)
+
+# DONE: Refine approach. Take care of edge cases.
 # EDGE CASE 1: obstacle is in way of target
 # Potential solution: go to another connected tile
-# EDGE CASE 2: target not detected after two additional scans.
-# EDGE CASE 3: second object detected in background
+# DONE CASE 2: target not detected after two additional scans.
+# DONE CASE 3: second object detected in background
+# EDGE CASE 4: target at a very bad angle
 
 """
 Change log
@@ -93,20 +121,31 @@ Change log
     -[0.0.2] Rishav
         --- Remove recursion as results not satisfying.
 """
-def approach(movement, pic_q, serial):
+def approach(movement, pic_q):
     movement.cam_up()
+    # Let camera catch up
+    time.sleep(2)
     # Get data of the closest object
     target_id, angle, inches = get_closest_target(pic_q)
-    if target_id == 0:
+    if target_id == 0 or inches > 13:
         print("Nothing found in approach")
+        # If nothing found in approach, check right and left 20 degrees. 
+        movement_list = [-20, 20]
+        for action in movement_list:
+            movement.turn(action)
+            # Let camera catch up
+            time.sleep(2)
+            target_id, angle, inches = get_closest_target(pic_q)
+            if target_id is not 0 and inches < 14:
+                print("Approach CAM_UP: [{}, {}, {}]".format(target_id, angle, inches))
+                approach_helper(angle, inches, pic_q, movement)
+                movement.turn(-1*action)
+                break
+            movement.turn(-1*action)
     else:
         print("Approach CAM_UP: [{}, {}, {}]".format(target_id, angle, inches))
-        # move towards the target
-        move_to_target(movement,-1*corrected_angle(angle, inches), inches)
-        #Realign with block
-        pick_up(movement, pic_q, serial)
-        # Reverse movement from the target
-        move_back_from_target(movement, corrected_angle(angle, inches), inches)
+        # call helper function which moves the robot
+        approach_helper(angle, inches, pic_q, movement)
 
 """
 Change log
@@ -382,10 +421,26 @@ def main():
     wait_for_button(buttonPin, ledPin)
     time.sleep(2)
     
-    print(movement.facing)
-    approach(movement, pic_q, ser)
-    print(movement.facing)
-                
+    approach(movement, pic_q)
+    pick_up_test = check_pick_up(movement, pic_q)
+    
+    """
+    drop_point = (4, 7)
+    targs = [(7, 0), (0, 7)]
+    
+    for item in targs:
+        movement.goal = item
+        follow_path(movement, pic_q)
+        approach(movement, pic_q, ser)
+        movement.goal = drop_point
+        follow_path(movement, pic_q)
+        movement.move(fwd, 6)
+        movement.drop()
+        movement.move(rev, 6)
+        movement.reset_servo()
+    #approach(movement, pic_q, ser)
+    #print(movement.facing)
+    """
     vt.join()
     #camera.close()
 
