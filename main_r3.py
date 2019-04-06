635b9dd30ad6417114f7d1d1373cf9d3c45faffc
Benji <pnhbenji@gmail.com>
2019-04-06 11:25:00 -0500

    Round 3 logic

--- main_r3.py
+++ main_r3.py
@@ -71,7 +71,7 @@ def main():
     
     # begin_round(movement, pic_q)
 
-    print("I will try and map the mothership")
+    # print("I will try and map the mothership")
     # MOTHERSHIP
     grid.mothership.extend([(),(),(),()])
     movement.set_access_point(())
@@ -79,84 +79,20 @@ def main():
     movement.set_side_point(())
     #OBSTACLES
     grid.obstacles.extend([])
-    # map_mothership(movement, pic_q)
-    print("Mothership is located in the following tiles: ", grid.mothership)
-
-    # We can save these values in movement.access_point so other functions with access to movement
-    # can use these
-    mothership_angle, dist, side_angle = approach_mothership_side(movement, pic_q, ser, GPIO)
-    movement.set_mothership_angle(mothership_angle)
-    movement.set_side_angle(side_angle)
-    movement.set_access_dist(dist)
-
-    print("Mothership angle: {}, Distance: {}, Side_angle: {}".format(mothership_angle, dist, side_angle))
-
-    print("Going home")
-    # go_home(movement, pic_q)
-    """
-    # pick up and drop blocks
-    for target in len(grid.targets):
-        movement.current_target = target
-        # movement.current_target = closest_point(grid.targets, movement.current)
-        movement.set_goal(movement.current_target)
-        follow_path(movement, pic_q)
-        approach(movement, pic_q)
-        checks = check_pick_up(movement, pic_q)
-        if checks[0]:
-            targetId = checks[1]
-            movement.move(fwd, 4)
-            movement.drop()
-            movement.move(rev, 4)
-            # grid.targets.remove(movement.current_target)
-            # kill_block(movement, pic_q)
-        else:
-            approach(movement, pic_q)
-            checks = check_pick_up(movement, pic_q)
-                if checks[0]:
-                    targetId = checks[1]
-                    movement.move(fwd, 4)
-                    movement.drop()
-                    movement.move(rev, 4)
-                    # grid.targets.remove(movement.current)
-                    # kill_block(movement, pic_q)
-        if not checks[0]:
-            movement.drop()
-
-        back_dat_ass_up(movement, pic_q)
-        go_home(movement,pic_q)
-    """
-    # now try to map mothership and drop blocks
-    # map_mothership(movement, pic_q)
-    # if not grid.mothership:
-    #     map_mothership(movement, pic_q)
-    # if not grid.mothership:
-    #     go_home(movement, pic_q)
-    #     wait_for_button(GPIO)
-    # else:
+    #TARGETS
+    grid.targets = [(),(),(),()]
     while grid.targets:
         movement.current_target = closest_point(grid.targets, movement.current)
         movement.set_goal(movement.current_target)
         follow_path(movement, pic_q,False,False)
         approach(movement, pic_q)
-        checks = check_pick_up(movement, pic_q)
-        if checks[0]:
-            targetId = checks[1]
-            grid.targets.remove(movement.current_target)
-        else:
-            approach(movement, pic_q)
-            checks = check_pick_up(movement, pic_q)
-                if checks[0]:
-                    targetId = checks[1]
-                    grid.targets.remove(movement.current_target)
-        back_dat_ass_up(movement, pic_q)
-        if not checks[0]:
-            go_home(movement, pic_q)
-        else:
-            movement.set_goal(movement.get_access_point())
-            follow_path(movement, pic_q, True, False)
-            movement.face(movement.get_side_point())
-            mothership_drop(dist, mothership_angle, side_angle, block_id, movement, serial, pic_q)
-            go_home(movement, pic_q)
+        grid.target.remove(movement.current_target)
+        # back_dat_ass_up(movement, pic_q)
+        movement.set_goal(movement.get_access_point())
+        follow_path(movement, pic_q, True, False)
+        movement.face(movement.get_side_point())
+        # drop code here
+        # mothership_drop(dist, mothership_angle, side_angle, block_id, movement, serial, pic_q)
 
 
     
